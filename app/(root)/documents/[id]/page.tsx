eeb74f9e739804f4582e8e834c16a43053682850
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-04-08 08:32:09 +0800

    Add Header component and integrate it into Document page; update global styles to enable dark theme

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,4 +1,16 @@
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+
 const Document = () => {
-  return <div>Document</div>;
+  return (
+    <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">Mock Title</p>
+        </div>{" "}
+      </Header>
+      <Editor />
+    </div>
+  );
 };
 export default Document;
