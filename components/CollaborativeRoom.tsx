23105d0863e145bb8e69c8dd1b49e64f9fda533b
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-04-08 15:13:52 +0800

    enhance document collaboration features; implement document retrieval, user authentication, and active collaborators display

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -1,25 +1,33 @@
 "use client";
 
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
-import { RoomProvider, ClientSideSuspense } from "@liveblocks/react";
+import { RoomProvider, ClientSideSuspense } from "@liveblocks/react/suspense";
 import { Editor } from "./editor/Editor";
 import Header from "./Header";
+import ActiveCollaborators from "./ActiveCollaborators";
+import Loader from "./Loader";
 
-const CollaborativeRoom = () => {
+const CollaborativeRoom = ({
+  roomId,
+  roomMetadata,
+}: CollaborativeRoomProps) => {
   return (
-    <RoomProvider id="my-room">
-      <ClientSideSuspense fallback={<div>Loading…</div>}>
+    <RoomProvider id={roomId}>
+      <ClientSideSuspense fallback={<Loader />}>
         <div className="collaborative-room">
           <Header>
             <div className="flex w-fit items-center justify-center gap-2">
               <p className="document-title">Share</p>
             </div>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+              <ActiveCollaborators />
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
           </Header>
           <Editor />
         </div>
